8ed264b10186a41d017bc806b53bfe2dd7f7f4f7
Edwin Perez <edwin.omar.perez@gmail.com>
2018-11-14 09:46:26 -0400

    Feature Wire Angular 7 with Electron (#7)
    * Removing all existing libs
    
    * Angular with Electron
    
    Getting everything wired - I bootstrapped Electron with Angular,
    removed unnecessary libs and cleaned up git mess.
    
    * Fixing foundation and rependencies loading.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'foundation-sites';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'toyoExpress';
+  ngOnInit () {
+    $(document).ready(() => {
+      $('#foundationVersion').html(Foundation.version);
+    });
+  }
 }

6004f548c1bae60b45b4f9de3fd04a9e179e1800
Edwin Perez <edwin.omar.perez@gmail.com>
2018-11-19 19:12:32 -0400

    updating DB to enable translations with multiples items (#13)
    updating DB to enable translations with multiples items

--- src/migrations/20181119180239_salesTransactions.ts
+++ src/migrations/20181119180239_salesTransactions.ts
@@ -1,10 +1,10 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<any> {
-    console.log('Creating table: sales');
-    return knex.schema.createTable('sales', (table) => {
+    console.log('Creating table: salesTransactions');
+    return knex.schema.createTable('salesTransactions', (table) => {
         table.increments('id');
-        table.integer('items_id').references('id').inTable('items');
+        table.integer('itemsSold_id').references('id').inTable('itemsSold');
         table.decimal('salesPrice').notNullable();
         table.decimal('taxesTotal');
         table.string('paymentMethod').notNullable();
@@ -14,7 +14,7 @@ export async function up(knex: Knex): Promise<any> {
         table.dateTime('salesDate').notNullable();
         table.dateTime('update_date');
         table.integer('users_id').references('id').inTable('users');
-        table.string('clientName');
+        table.string('clientNam');
         table.string('vinNumber');
         table.string('vModel');
         table.string('vModelYear');
@@ -22,7 +22,7 @@ export async function up(knex: Knex): Promise<any> {
 }
 
 export async function down(knex: Knex): Promise<any> {
-    console.log('Rollingback table: sales');
-    return knex.schema.dropTable('sales');
+    console.log('Rollingback table: salesTransactions');
+    return knex.schema.dropTable('salesTransactions');
 }
 
